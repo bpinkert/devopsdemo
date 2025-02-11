2b1a0e496b149593288cdcfa7687b7f52cab6e62
Benjamin Pinkert <benjamin.pinkert@pditechnologies.com>
2025-02-11 10:37:22 -0500

    first

---
+++ lambda.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# A simple route for the main page
+@app.route('/')
+def home():
+    # Render an HTML template with some dynamic data
+    name = "John Doe"
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
